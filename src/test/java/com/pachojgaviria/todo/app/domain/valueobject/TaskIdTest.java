0aeccdc7894db1d877270f5b754ed466999fbc5d
Johnatan Garzon <johnatan.garzonm@perficient.com>
2020-12-10 14:50:00 -0500

    add tastkIdTest

--- src/test/java/com/pachojgaviria/todo/app/domain/valueobject/TaskIdTest.java
+++ src/test/java/com/pachojgaviria/todo/app/domain/valueobject/TaskIdTest.java
@@ -1,5 +1,64 @@
 package com.pachojgaviria.todo.app.domain.valueobject;
 
+import org.junit.jupiter.api.Test;
+
+import com.github.javafaker.Faker;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TaskIdTest {
 
+  @Test
+  public void shouldFailWhenIdIsNull() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      //given
+      var id = (String) null;
+      //when
+      new TaskId(id);
+    });
+  }
+
+  @Test
+  public void shouldFailWhenIdIsEmpty() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      //given
+      var id = "";
+      //when
+      new TaskId(id);
+    });
+  }
+
+  @Test
+  public void shouldFailWhenIdIsBlank() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      //given
+      var id = "  ";
+      //when
+      new TaskId(id);
+    });
+  }
+
+  @Test
+  public void shouldFailWhenIdIsNotUuid() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      //given
+      var faker = new Faker();
+      var id = faker.rickAndMorty().character();
+      //when
+      new TaskId(id);
+    });
+  }
+
+  @Test
+  public void shouldCreateTaskId() {
+    //given
+    var id = UUID.randomUUID().toString();
+    //when
+    var taskId = new TaskId(id);
+    //then
+    assertNotNull(taskId);
+    assertEquals(id, taskId.getId());
+  }
 }
